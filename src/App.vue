64e662655a8617f051a3317c42874067a1e63515
ramadanko <mohammed.ramadanko@gmail.com>
2024-06-10 09:45:48 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon />
+      </template>
+      <v-app-bar-title>Logo</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer></v-navigation-drawer>
+    <v-main>
+
+      <!--      <RouterView />-->
+    </v-main>
+  </v-app>
+</template>
