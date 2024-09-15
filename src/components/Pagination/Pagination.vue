7d8f3f55615e923b2d8329784dc798696338a7ea
ramadanko <mohammed.ramadanko@gmail.com>
2024-09-15 14:58:41 +0300

    add Pagination component

--- src/components/Pagination/Pagination.vue
+++ src/components/Pagination/Pagination.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useRouteQuery } from '@vueuse/router'
 import { useRouter, useRoute } from 'vue-router'
+import { computed } from 'vue'
 
 const router = useRouter()
 const route = useRoute()
-const { pageNumberKey, pageSizeKey } = defineProps({
+const { pageNumberKey, pageSizeKey, totalItems } = defineProps({
   pageNumberKey: {
     type: String,
     default: 'pageNumber'
@@ -12,12 +13,17 @@ const { pageNumberKey, pageSizeKey } = defineProps({
   pageSizeKey: {
     type: String,
     default: 'pageSize'
+  },
+  totalItems: {
+    type: Number,
+    default: 0
   }
 })
 
 const pageSizeOptions = [1, 5, 10, 25, 50]
 const pageSize = useRouteQuery(pageSizeKey, '10', { transform: Number })
 const pageNumber = useRouteQuery(pageNumberKey, '1', { transform: Number })
+const length = computed(() => Math.ceil(totalItems / pageSize.value))
 
 const onPageSizeChange = (value: number) => router.push({ query: { ...route.query, pageNumber: 1, pageSize: value } })
 
@@ -25,8 +31,15 @@ const onPageNumberChange = (value: number) => router.push({ query: { ...route.qu
 
 </script>
 <template>
-
-  <v-pagination :length="5" @update:model-value="onPageNumberChange" :model-value="pageNumber" />
-  <v-select label="Page Size" :items="pageSizeOptions" variant="outlined" @update:model-value="onPageSizeChange"
-            :model-value="pageSize" max-width="100px" />
+  <v-sheet class="d-flex">
+    <v-col>
+      <v-select density="compact" label="Page Size" :items="pageSizeOptions" variant="outlined" max-width="100px"
+                @update:model-value="onPageSizeChange" :model-value="pageSize" />
+    </v-col>
+    <v-spacer />
+    <v-col>
+      <v-pagination density="compact" v-if="length > 1" :length="length" @update:model-value="onPageNumberChange"
+                    :model-value="pageNumber" />
+    </v-col>
+  </v-sheet>
 </template>
